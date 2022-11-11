4200f88df189e26f9b7a625b6d919274a69d3bd3
feidian8352 <43678136+feidian8352@users.noreply.github.com>
2022-11-11 16:34:34 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "98626fb4-aa85-4c7d-81f1-ea6eb03c150c",
                         "alterId": 0
                     }
                 ],
